75e5f045cd36e159fed416cf7d75374facd12af7
Chaitanya Gosavi <chaitanyagosavi540@gmail.com>
2023-05-31 01:22:34 +0530

    toastify

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,9 +1,18 @@
+import { auth } from "@/firebase/firebase";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Logout from "../Buttons/Logout";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "@/atoms/authModalAtom";
+import { error } from "console";
 
 type Props = {};
 
 const Topbar = (props: Props) => {
+  const [user] = useAuthState(auth);
+  const setAuthModalState = useSetRecoilState(authModalState);
+
   return (
     <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
       <div
@@ -24,11 +33,40 @@ const Topbar = (props: Props) => {
               Premium
             </a>
           </div>
-          <Link href="/auth">
-            <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
-              Sign In
-            </button>
-          </Link>
+          {user ? (
+            <div className="cursor-pointer group relative">
+              <img
+                src="/avatar.png"
+                alt="Avatar"
+                width={30}
+                height={30}
+                className="rounded-full"
+              />
+              <div
+                className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
+             z-40 group-hover:scale-100 scale-0 
+             transition-all duration-300 ease-in-out"
+              >
+                <p className="text-sm">{user.email}</p>
+              </div>
+            </div>
+          ) : (
+            <Link
+              href="/auth"
+              onClick={() =>
+                setAuthModalState((prev) => ({
+                  ...prev,
+                  isOpen: true,
+                  type: "login",
+                }))
+              }
+            >
+              <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
+                Sign In
+              </button>
+            </Link>
+          )}
+          {user ? <Logout /> : <></>}
         </div>
       </div>
     </nav>
